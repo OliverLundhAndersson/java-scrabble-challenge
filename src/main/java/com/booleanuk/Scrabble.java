9d45a679abdddf1c4773234e80d005bbdd0bdd61
Oliver Andersson <oliverlundhandersson@gmail.com>
2024-08-13 13:38:59 +0200

    Exercise extension done

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -5,25 +5,56 @@ import java.util.HashMap;
 public class Scrabble {
     HashMap<Character, Integer> letterScores = new HashMap<>();
     String word;
-    //Add scores
 
     public Scrabble(String word) {
         this.word = word.toUpperCase();
         setLetterScores();
-        score();
     }
 
     public int score() {
+        //Score word
         int points = 0;
         for (int i = 0; i < this.word.length(); i++) {
-            if (this.letterScores.containsKey(word.charAt(i))) {
+            //Check for correct double points
+            if (word.charAt(i) == '{') {
+                if (word.length() > i+2 && word.charAt(i+2) == '}') {
+                    Scrabble scrabble = new Scrabble(word.substring(i+1, i+2));
+                    points += scrabble.score() * 2;
+                    i += 2;
+                } else if (word.charAt(word.length()-1-i) == '}' && word.length()-1-i > i) {
+                    Scrabble scrabble = new Scrabble(word.substring(i+1, word.length()-1-i));
+                    points += scrabble.score() * 2;
+                    i = word.length()-1-i;
+                } else {
+                    return 0;
+                }
+            }
+            //Check for correct triple points
+            else if (word.charAt(i) == '[') {
+                if (word.length() > i+2 && word.charAt(i+2) == ']') {
+                    Scrabble scrabble = new Scrabble(word.substring(i+1, i+2));
+                    points += scrabble.score() * 3;
+                    i += 2;
+                } else if (word.charAt(word.length()-1-i) == ']' && word.length()-1-i > i) {
+                    Scrabble scrabble = new Scrabble(word.substring(i+1, word.length()-1-i));
+                    points += scrabble.score() * 3;
+                    i = word.length()-1-i;
+                } else {
+                    return 0;
+                }
+            }
+            //Check for letter and its score
+            else if (this.letterScores.containsKey(word.charAt(i))) {
                 points += letterScores.get(word.charAt(i));
+            } else {
+                return 0;
             }
         }
         return points;
     }
 
     public void setLetterScores() {
+        //Set all letter scores
         this.letterScores.put('A', 1);
         this.letterScores.put('E', 1);
         this.letterScores.put('I', 1);
